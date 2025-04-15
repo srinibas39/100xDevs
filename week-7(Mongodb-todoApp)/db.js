4a73580230212c45fd927b49ea48acc376f8f07d
srinibas khuntia <srinibas.k@pillir.io>
2025-04-16 00:57:05 +0530

    added timestamps

--- week-7(Mongodb-todoApp)/db.js
+++ week-7(Mongodb-todoApp)/db.js
@@ -6,14 +6,22 @@ const ObjectId = mongoose.ObjectId;
 const User = new Schema({
     name:String,
     email:{type:String,unique:true},
-    password:String
-})
+    password:String,
+},
+    {
+        timestamps : true
+    }
+)
 
 const Todo = new Schema({
     userId:ObjectId,
     title:String,
     done:Boolean
-})
+},
+{
+    timestamps:true
+}
+)
 
 const UserModel = mongoose.model("User",User)
 const TodoModel = mongoose.model("Todo",Todo)
