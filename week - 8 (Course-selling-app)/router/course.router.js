5626fb9186f93bb04c05f9a6b20ef9db9e28214d
srinibas khuntia <srinibas.k@pillir.io>
2025-04-16 13:10:58 +0530

    set up the routers for coursify

---
+++ week - 8 (Course-selling-app)/router/course.router.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const courseRouter = express.Router();
+
+courseRouter.post("/courses",(req,res)=>{
+
+})
+
+courseRouter.get("/preview",(req,res)=>{
+
+})
+
+module.exports = {
+    courseRouter:courseRouter
+}
---
+++ week - 8 (Course-selling-app)/router/user.router.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const userRouter = express.router();
+
+userRouter.post("signup",(req,res)=>{
+
+})
+
+userRouter.post("signin",(req,res)=>{
+    
+})
+
+userRouter.get("purchases",(req,res)=>{
+    //For course purchases
+})
+
+module.exports = {
+    userRouter : userRouter
+}
+
