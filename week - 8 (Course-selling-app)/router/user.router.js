01e0ab85de48ec84567acff034bb46b1e80d2e6a
srinibas khuntia <srinibas.k@pillir.io>
2025-04-17 12:28:39 +0530

    user signin and signup endpoint done

--- week - 8 (Course-selling-app)/router/user.router.js
+++ week - 8 (Course-selling-app)/router/user.router.js
@@ -1,8 +1,39 @@
 const express = require("express");
+const {z} = require("zod");
+const { userModel } = require("../db");
 const userRouter = express.Router();
 
-userRouter.post("/signup",(req,res)=>{
+userRouter.post("/signup",async(req,res)=>{
+    try{
+        const userSchema = z.object({
+            email:z.string().email(),
+            password:z.string().min(3).max(100),
+            firstName:z.string(),
+            lastName:z.string()
+        })
 
+        const parsedData = userSchema.safeParse(req.body);
+        if(parsedData.success){
+           
+            await userModel.create({
+                ...parsedData.data
+            })
+
+            res.json({
+                message:"successfully signed up"
+            })
+        }
+        else{
+            res.json({
+                message:"Unable to authenticate"
+            }).status(403)
+        }
+    }
+    catch(e){
+        res.json({
+            message:"Internal Server error"
+        }).status(500)
+    }
 })
 
 userRouter.post("/signin",(req,res)=>{
