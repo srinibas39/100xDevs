04d99729d0767c595feec9973703d4661e04ec10
srinibas khuntia <srinibas.k@pillir.io>
2025-04-17 18:35:36 +0530

    added auth middlewares in admin and user

---
+++ week - 8 (Course-selling-app)/middleware/user.middleware.js
@@ -0,0 +1,33 @@
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
+
+const jwtSecret = process.env.JWT_SECRET_KEY_USER
+
+function auth(req,res,next){
+    try{
+        const {token} = req.headers.authorization;
+        if(!token){
+            return res.json({
+                message:"token does not exist"
+            })  
+        }
+
+        const response = jwt.verify(token,jwtSecret)
+        if(!response){
+            return res.json({
+                message:"token does not exist"
+            })  
+        }
+        req.id = response.id
+        next()
+    }
+    catch(err){
+        res.status(500).json({
+            message:"Internal server error"
+        })
+    }
+}
+
+module.exports = {
+    auth:auth
+}
