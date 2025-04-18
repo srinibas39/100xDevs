453b9b5b07c8324a43ab13c17303ec9badba5541
srinibas khuntia <srinibas.k@pillir.io>
2025-04-18 18:19:24 +0530

    implemented course model

--- week - 8 (Course-selling-app)/router/course.router.js
+++ week - 8 (Course-selling-app)/router/course.router.js
@@ -1,12 +1,43 @@
 const express = require("express");
+const { auth } = require("../middleware/user.middleware");
+const { purchaseModel, courseModel } = require("../db");
 const courseRouter = express.Router();
 
-courseRouter.post("/courses",(req,res)=>{
+courseRouter.get("/courses/:courseId",auth,async(req,res)=>{
+    try{
+        const id = req.id;
+        const  courseId = req.params.courseId
+    
+        //integrate the payment and check if payment is made
+        await purchaseModel.create({
+            userId:id,
+            courseId:courseId
+        });
+    
+        res,json({
+            message : "you have successfully purchased the courses"
+        })
+    }
+    catch(e){
+        res.status(500).json({
+            message:"Internal server error"
+        })
+    }
 
 })
 
-courseRouter.get("/preview",(req,res)=>{
-
+courseRouter.get("/preview",async(req,res)=>{
+  try{
+      const allCourses =  await courseModel.find({});
+      res.json({
+        courses:allCourses
+      })
+  }
+  catch(err){
+    res.status(500).json({
+        message:"Internal server error"
+    })
+  }
 })
 
 
