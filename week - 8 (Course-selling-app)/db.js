c87405ea98670ea1e8a155b62f1247ef6d24f38f
srinibas khuntia <srinibas.k@pillir.io>
2025-04-18 14:08:46 +0530

    add db schema

---
+++ week - 8 (Course-selling-app)/db.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const objectId = mongoose.objectId;
+
+const userSchema = new Schema({
+    firstName:String,
+    lastName:String,
+    email:String,
+    password:String
+},
+{
+    timestamps:true
+})
+
+const adminSchema = new Schema({
+    firstName:String,
+    lastName:String,
+    email:String,
+    password:String,
+},{
+    timestamps:true
+})
+
+const courseSchema = new Schema({
+    title:String,
+    description:String,
+    imageUrl:String,
+    price:String,
+    creatorId:objectId
+},{
+    timestamps:true
+})
+
+const purchaseSchema = new Schema({
+     userId:objectId,
+     courseId:objectId
+},
+{
+    timestamps:true
+})
+
+const userModel = mongoose.model("user",userSchema);
+const adminModel = mongoose.model("admin",adminSchema);
+const courseModel = mongoose.model("course",courseSchema);
+const purchaseModel = mongoose.model("purchase",purchaseSchema);
+
+
+module.exports = {
+    userModel : userModel,
+    adminModel : adminModel,
+    courseModel : courseModel,
+    purchaseModel:purchaseModel
+}
+
+
