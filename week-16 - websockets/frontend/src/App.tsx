53e13d14a4f6e62865ef105a8892de2ca0133020
srinibas khuntia <srinibas.k@pillir.io>
2025-04-26 16:44:29 +0530

    a simple ws server established

--- week-16 - websockets/frontend/src/App.tsx
+++ week-16 - websockets/frontend/src/App.tsx
@@ -1,15 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [socket,setSocket] = useState<WebSocket | null>(null)
+  const [message,setMessage] = useState<string>("")
+  const handleSend = ()=>{
+    // socket.send(e.target.value)
+    if(socket?.readyState === WebSocket.OPEN){
+      socket?.send(message)
+    }
+  }
+
+  useEffect(()=>{
+    const ws = new WebSocket("ws://localhost:5000")
+    setSocket(ws)
+    ws.onopen = ()=>{
+      console.log("websocket connection established")
+    }
+    ws.onmessage = (e)=>{
+        // alert(e.data)
+        console.log(e.data)
+    }
+    ws.onerror = (e)=>{
+      console.error("Webscoket error",e)
+    }
+
+    return ()=>{
+      ws.close()
+    }
+  },[])
 
   return (
     <>
       <div>
-         hello web sockets
+           <h1>Web sockets</h1>
+           <label>Enter message</label>
+           <input type="text" name="message" onChange={e=>setMessage(e.target.value)} value={message}/>
+           <button onClick={handleSend} >Send</button>
       </div>
     </>
   )
