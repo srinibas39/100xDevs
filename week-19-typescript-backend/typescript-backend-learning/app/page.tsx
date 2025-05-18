930dbf179e6f223c27d96b052addafc28926a743
Srinibas Khuntia <srinibaskhuntia39@gmail.com>
2025-05-18 22:06:09 +0530

    created ui and endpint for signin

--- week-19-typescript-backend/typescript-backend-learning/app/page.tsx
+++ week-19-typescript-backend/typescript-backend-learning/app/page.tsx
@@ -1,4 +1,5 @@
 import axios from "axios"
+import Link from "next/link";
 
 
 async function getUser(){
@@ -16,13 +17,18 @@ export default async function Home() {
   const userData = await getUser()
   return (
     <div className="flex flex-col justify-center h-screen">
-        <div className="flex justify-center">
+        <div className="flex justify-center align-center">
             <div className="border p-8 rounded">
                 <div>
                     Name: {userData?.name}
                 </div>
                 
                 {userData?.email}
+            </div>
+            <div className="border p-8 rounded">
+                <Link href="/signup"  >Signup</Link>
+                <div className="m-4"></div>
+                <Link href="/signin">Signin</Link>
             </div>
         </div>
     </div>
---
+++ week-19-typescript-backend/typescript-backend-learning/app/signup/page.tsx
@@ -0,0 +1,5 @@
+import { SignupComp } from "../components/signup";
+
+export default function Signup(){
+    return <SignupComp/>
+}
